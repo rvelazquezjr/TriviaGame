f08d6f44e67c9dda770c11b2cbd68427b579833d
Enterprise D <51425632+rvelazquezjr@users.noreply.github.com>
2019-09-08 14:07:14 -0400

    more lovely js/jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,12 +5,35 @@ let score = 0;
 let lost = 0;
 let timer;
 
+function countDown() {
+    counter--;
+}
+
 function loadQuestion() {
+    counter = 30;
+    timer = setInterval(countDown, 1000);
 
     const question = quizQuestions[currentQuestion].question;
     const choices = quizQuestions[currentQuestion].choices;
+
+    $('#time').html('Timer: ' + counter);
+    $('#game').html(`
+        <h4>${question}</h4>
+        ${loadChoices(choices)}
+    `);
 }
 
+function loadChoices(choices) {
+    let result = '';
+
+    for (let i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+
+    return result;
+}
+
+loadQuestion();  
 
 
 
