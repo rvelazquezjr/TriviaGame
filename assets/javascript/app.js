fbcac0d6d1fde944f256de82494b2c298bc1f9fe
Enterprise D <51425632+rvelazquezjr@users.noreply.github.com>
2019-09-08 13:15:40 -0400

    now some real js

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+let counter = 30;
+let currentQuestion = 0;
+let score = 0;
+let lost = 0;
+let timer;
+
+function loadQuestion() {
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+}
+
+
+
+
