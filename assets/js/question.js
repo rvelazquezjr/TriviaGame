c15e68404b53fa68c07e168c5de72391862339be
Ralph Velazquez <rvelazquez30@icloud.com>
2021-03-04 21:01:02 -0500

    cooler automaker answer

---
+++ assets/js/question.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+    {
+        question: 'Which drink burns a little?',
+        choices: ['Coffee', 'Soda', 'Fruit Juice', 'Tea'],
+        correctAnswer: 'Soda'
+    },
+
+    {
+        question: 'Which automaker do you NEVER see advertisements for?',
+        choices: ['Ford', 'Honda', 'Volvo', 'Tesla'],
+        correctAnswer: 'Tesla'
+    },
+
+    {
+        question: 'How many feet are in a mile?',
+        choices: ['2,580', '4,280', '5,280', 'A BaJillion'],
+        correctAnswer: '5,280'
+    },
+
+    {
+        question: 'What is my favorite season of the year?',
+        choices: ['Winter', 'Summer', 'Fall', 'Spring'],
+        correctAnswer: 'Winter'
+    }
+];
+
+
